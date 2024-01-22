23cd6f4c09c5f46c07ebc76414d8091a0a616ced
Hicham010 <47334105+Hicham010@users.noreply.github.com>
2024-01-22 13:58:12 +0100

    Updates

--- assets/index-iSsFXw5-.js
+++ assets/index-iSsFXw5-.js
@@ -1,4 +1,4 @@
-import{n as ve,s as qe,T as k,t as W,a as u,o as Q,R as jr,p as U,y as ne}from"./index-6YQZiI4Y.js";import{b as Nr}from"./browser-0P6RYkfe.js";import"./index-CjTQ73lM.js";/**
+import{n as ve,s as qe,T as k,t as W,a as u,o as Q,R as jr,p as U,y as ne}from"./index-9sdovEVm.js";import{b as Nr}from"./browser-HDjXZcvq.js";import"./index-pvguUcgP.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
